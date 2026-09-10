6d862477cc1593a1feb83df54cd1bca8da5b233d
M-Abdullah-Bhatti <87266652+M-Abdullah-Bhatti@users.noreply.github.com>
2026-09-10 19:37:41 +0500

    fix: send dob field so date of birth persists

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -174,7 +174,7 @@ async findByEmail(email: string): Promise<User | null> {
         {
           new: true,
           select:
-            'email firstName lastName username profileImage bio gender followerCount followingCount videoCount role',
+            'email firstName lastName username profileImage bio gender dob followerCount followingCount videoCount role',
         },
       )
       .exec();
